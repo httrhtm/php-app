1f7fe192dbeeead55b29e241adcc96e19f8d894a
httrhtm <h_hattori@emyus.jp>
2021-04-21 10:08:17 +0900

    答えを1つだけ登録する処理を追加

--- insert.php
+++ insert.php
@@ -4,14 +4,28 @@ require 'db_connection.php';
 
 // 変数に入れる
 $question = $_POST['question'];
-$answers = $_POST['answer']; //配列
+$answer = $_POST['answer']; //配列
 
 // --------------------------------------------------
 // 問題の登録
 // --------------------------------------------------
-$sql = 'insert into questions (question, created_at) values (:question, current_timestamp())';
+$sql = 'insert into questions (question, created_at)
+            values (:question, current_timestamp())';
 $stmt = $db->prepare($sql);
 
 //実行
 $stmt->execute([':question' => $question]);
+
+// --------------------------------------------------
+// 答えの登録
+// --------------------------------------------------
+$sql = 'insert into correct_answers (questions_id, answer, created_at)
+            values ((select id from questions order by created_at desc limit 1),
+                    :answer,
+                    current_timestamp())';
+
+$stmt = $db->prepare($sql);
+
+//実行
+$stmt->execute([':answer' => $answer]);
 ?>
