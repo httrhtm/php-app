5d5e264d6fa687de18550d87dc66767d25eaa007
httrhtm <h_hattori@emyus.jp>
2021-04-22 16:46:20 +0900

    編集画面のバリデーション

--- edit.php
+++ edit.php
@@ -24,6 +24,39 @@ $answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 // --------------------------------------------------
 // バリデーション
 // --------------------------------------------------
+session_start();
+if (!empty($_POST) && empty($_SESSION['input_data'])) {
+
+    $question = $_POST['question'];
+    $answers = $_POST['answer'];
+
+    //問題
+    if (empty($question)) {
+        $error_message['question'] = '問題を入力して下さい';
+    } elseif (mb_strlen($question) > 500) {
+        $error_message['question'] = '問題を500文字以内で入力してください';
+    }
+
+    //答え
+    if (empty($answers['answer'])) {
+        $error_message['answer'] = '答えを入力して下さい';
+    } elseif (mb_strlen($answers['answer']) > 200) {
+        $error_message['answer'] = '答えを200文字以内で入力してください';
+    }
+
+
+
+    //エラー内容チェック -- エラーがなければregister_confirm.phpへリダイレクト
+    if (empty($error_message)) {
+        $_SESSION['input_data'] = $_POST;
+        header('Location:./register_confirm.php', true, 307);
+        exit();
+    }
+} elseif (!empty($_SESSION['input_data'])) {
+    $_POST = $_SESSION['input_data'];
+}
+
+session_destroy();
 
 ?>
 
@@ -41,7 +74,9 @@ $answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	<!-- register -->
 	<div class="edit">
 
-		<form action="edit_confirm.php" method="post" autocomplete="off">
+
+		<form action="edit.php" method="post" autocomplete="off">
+
 			<!-- 問題 -->
 
 			<table>
@@ -54,6 +89,9 @@ $answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 				</tr>
 			</table>
+			<span>
+				<?php echo isset($error_message['question']) ? $error_message['question'] : ''; ?>
+			 </span>
 
 			<!-- 答え -->
 			<?php
@@ -72,9 +110,16 @@ $answers = $stmt->fetchAll(PDO::FETCH_ASSOC);
 					</td>
 				</tr>
 			</table>
+
+			<span>
+				<?php echo isset($error_message['answer']) ? $error_message['answer'] : ''; ?>
+			 </span>
+
 			<?php
             }
             ?>
+
+			<br>
 			<button type="button" onclick="location.href='list.php'">戻る</button>
 			<button>追加*</button>
 			<button type="submit">確認</button>
