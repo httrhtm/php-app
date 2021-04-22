ff8400d414de4eef96be55e72284560847a56dee
httrhtm <h_hattori@emyus.jp>
2021-04-22 15:39:30 +0900

    新規登録時のバリデーションを追加

--- register.php
+++ register.php
@@ -1,3 +1,42 @@
+<?php
+    session_start();
+    if (!empty($_POST) && empty($_SESSION['input_data'])) {
+
+        $question = $_POST['question'];
+        $answers = $_POST['answer'];
+
+        //問題
+        if (empty($question)) {
+            $error_message['question'] = '問題を入力して下さい';
+        } elseif (mb_strlen($question) > 500) {
+            $error_message['question'] = '問題を500文字以内で入力してください';
+        }
+
+        //答え
+        foreach ($answers as $answer) {
+            if (empty($answer)) {
+                $error_message['answer'] = '答えを入力して下さい';
+            } elseif (mb_strlen($answer) > 200) {
+                $error_message['answer'] = '答えを200文字以内で入力してください';
+            }
+
+        }
+
+        //エラー内容チェック -- エラーがなければregister_confirm.phpへリダイレクト
+        if (empty($error_message)) {
+            $_SESSION['input_data'] = $_POST;
+            header('Location:./register_confirm.php', true, 307);
+            exit();
+        }
+    } elseif (!empty($_SESSION['input_data'])) {
+        $_POST = $_SESSION['input_data'];
+    }
+
+    session_destroy();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -12,25 +51,29 @@
 	<!-- register -->
 	<div class="register">
 
-		<form action="register_confirm.php" method="post">
+		<form action="register.php" method="post">
 			<!-- 問題 -->
 
 			<table>
 				<tr>
 					<th>問題:</th>
 					<td>
-						<input name="question">
+						<input name="question"
+						value="<?php echo isset($question) ? htmlspecialchars($question,ENT_QUOTES) : ''; ?>">
 					</td>
-
 				</tr>
 			</table>
+			<span>
+				<?php echo isset($error_message['question']) ? $error_message['question'] : ''; ?>
+			 </span>
 
 			<!-- 答え -->
 			<table>
 				<tr>
 					<th>答え:</th>
 					<td>
-						<input name="answer[]">
+						<input name="answer[]"
+						value="<?php echo isset($answer) ? htmlspecialchars($answer,ENT_QUOTES) : ''; ?>">
 					</td>
 
 					<td>
@@ -39,13 +82,17 @@
 
 				</tr>
 			</table>
+			<span>
+				<?php echo isset($error_message['answer']) ? $error_message['answer'] : ''; ?>
+			 </span>
 
 			<!-- 答え -->
 			<table>
 				<tr>
 					<th>答え:</th>
 					<td>
-						<input name="answer[]">
+						<input name="answer[]"
+						value="<?php echo isset($answer) ? htmlspecialchars($answer,ENT_QUOTES) : ''; ?>">
 					</td>
 
 					<td>
@@ -54,10 +101,14 @@
 
 				</tr>
 			</table>
+			<span>
+				<?php echo isset($error_message['answer']) ? $error_message['answer'] : ''; ?>
+			 </span>
 
+			<br>
 			<button type="button" onclick="location.href='list.php'">戻る</button>
 			<button>追加*</button>
-			<button type="submit" name ="btn_submit">確認</button>
+			<button type="submit">確認</button>
 
 		</form>
 
