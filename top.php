4e2752fea110939e6f529e5b2f09b6730214783a
httrhtm <h_hattori@emyus.jp>
2021-04-26 14:14:58 +0900

    admin_flagで表示内容を変えるように修正

--- top.php
+++ top.php
@@ -1,3 +1,20 @@
+<?php
+session_start();
+require 'db_connection.php';
+// --------------------------------------------------
+// ログインユーザーを取得する
+// --------------------------------------------------
+$user_id = $_SESSION['id'];
+// --------------------------------------------------
+// ユーザーをDBから取得する
+// --------------------------------------------------
+if (isset($db)) {
+    $sql = 'select admin_flag from users where id = :id';
+    $stmt = $db->prepare($sql);
+    $stmt->execute([':id' => $user_id]);
+    $user_admin = $stmt->fetch(PDO::FETCH_COLUMN);
+}
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -14,9 +31,15 @@
 	</div>
 
 	<div class="main-nav">
+		<?php
+		if (strcmp($user_admin, '1') == 0) {
+		?>
 		<form action="list.php" method="post">
 			<button type="submit">問題と答えを確認・登録する　＞</button>
 		</form>
+		<?php
+		}
+		?>
 
 		<form action="test.php" method="post">
 			<button type="submit">テストをする　＞</button>
@@ -25,6 +48,16 @@
 		<form action="history.php" method="post">
 			<button type="submit">過去の採点結果をみる　＞</button>
 		</form>
+
+		<?php
+		if (strcmp($user_admin, '1') == 0) {
+		?>
+		<form action="user_lists.php" method="post">
+			<button type="submit">ユーザを追加・編集する　＞</button>
+		</form>
+		<?php
+		}
+		?>
 	</div>
 </body>
 </html>
